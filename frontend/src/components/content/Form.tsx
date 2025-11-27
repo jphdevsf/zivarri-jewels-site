27a83e89055d50d9a8c0478e08b0616362f3ef92
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2025-11-27 09:04:58 -1000

    refine email template, increase outlook client compatibility, inline css, etc.

--- frontend/src/components/content/Form.tsx
+++ frontend/src/components/content/Form.tsx
@@ -32,8 +32,8 @@ const Form = ({ title }: FormBlock) => {
       setLoading(false)
     }
   }
-  const shortInputClassList = 'w-full max-w-sm mb-4 px-3 py-2 dark:bg-stone-600 bg-stone-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 border-gray-300'
-  const textAreaClassList = 'w-full max-w-xl mb-4 px-3 py-2 dark:bg-stone-600 bg-stone-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 border-gray-300'
+  const shortInputClassList = 'w-full max-w-sm mb-4 px-3 py-4 dark:bg-stone-600 bg-stone-200 focus:outline-none focus:ring-2 focus:ring-blue-500 border-gray-300'
+  const textAreaClassList = 'w-full max-w-sm mb-4 px-3 py-4 dark:bg-stone-600 bg-stone-200 focus:outline-none focus:ring-2 focus:ring-blue-500 border-gray-300'
   const labelClassList = 'block text-sm font-medium my-2 text-primary dark:text-primary-dark'
   const errorClassList = 'block text-xs mb-8 text-red-900 dark:text-red-400'
   return (
@@ -45,9 +45,9 @@ const Form = ({ title }: FormBlock) => {
         className="content-form text-primary block px-8 py-12 dark:text-primary-dark bg-background dark:bg-background-dark"
       >
         <label className={labelClassList}>Name</label>
-        <input {...register('name', { required: true, pattern: /^[A-Za-z]+$/i })} className={shortInputClassList} />
+        <input {...register('name', { required: true, pattern: /^[a-z]{1}[a-z\s]+$/i })} className={shortInputClassList} />
         {errors.name && errors.name.type === 'required' && <span className={errorClassList}>Name is required.</span>}
-        {errors.name && errors.name.type === 'pattern' && <span className={errorClassList}>Only letters are allowed in this field.</span>}
+        {errors.name && errors.name.type === 'pattern' && <span className={errorClassList}>Only letters and spaces are allowed in this field.</span>}
 
         <label className={labelClassList}>Email</label>
         <input {...register('email', { required: true, pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/ })} className={shortInputClassList} />
@@ -58,7 +58,7 @@ const Form = ({ title }: FormBlock) => {
         <textarea
           {...register('message', { required: true, maxLength: 1000 })}
           className={textAreaClassList}
-          rows={4}
+          rows={6}
         />
         {errors.message && errors.message.type === 'required' && <span className={errorClassList}>Please include a message in your inquiry.</span>}
         {errors.message && errors.message.type === 'maxLength' && <span className={errorClassList}>Please limit your message to 1000 characters or less.</span>}
@@ -66,16 +66,17 @@ const Form = ({ title }: FormBlock) => {
         <button
           type="submit"
           disabled={loading}
-          className="content-form-button block relative text-background bg-primary dark:text-background dark:bg-primary border hover:text-primary hover:bg-background transition-all duration-300 text-center text-md py-3 px-8 z-20 my-2 opacity-50 cursor-not-allowed disabled:pointer-events-none"
+          className={`content-form-button block text-background bg-primary dark:text-background-dark dark:bg-primary-dark relative border hover:text-primary hover:bg-background transition-all duration-300 text-center text-md py-3 px-8 z-20 my-2 ${loading && 'opacity-50 cursor-not-allowed disabled:pointer-events-none'}`}
         >
           {loading ? 'Sending...' : 'Submit'}
         </button>
+        {message && (
+          <p className='mt-4 text-confirmation dark:text-confirmation-dark text-left'>
+            {message}
+          </p>
+        )}
       </form>
-      {message && (
-        <p className="mt-4 px-8 text-green-600 dark:text-green-400 text-center">
-          {message}
-        </p>
-      )}
+
     </>
   )
 }
