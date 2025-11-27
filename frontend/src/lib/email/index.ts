fd2c43c25d3170c7066d0f5e82247aa33ad25ce4
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2025-11-27 09:31:19 -1000

    add dev only logic to nodemailer integration that creates test user with Ethereal in dev environment. Also set up env variable to for forcing production behavior temporarily for testing

--- frontend/src/lib/email/index.ts
+++ frontend/src/lib/email/index.ts
@@ -7,15 +7,33 @@ const sendEmail = async (data: FormData) => {
   const templatePath = path.join(process.cwd(), 'src/lib/email/email-template-submission.html')
   const emailTemplate = fs.readFileSync(templatePath, 'utf8')
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT || '587'),
-    secure: false,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
-    }
-  })
+  const useProductionTransporter = process.env.NODE_ENV !== 'development' || process.env.FORCE_PRODUCTION_SMTP === 'true'
+
+  let transporter
+
+  if (!useProductionTransporter) {
+    // Ethereal for dev (per Nodemailer docs: https://nodemailer.com/usage/#ethereal-email)
+    const testAccount = await nodemailer.createTestAccount()
+    transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    })
+  } else {
+    transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT || '587'),
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+      }
+    })
+  }
 
   const html = emailTemplate
     .replace('{{NAME}}', data.name)
@@ -23,13 +41,19 @@ const sendEmail = async (data: FormData) => {
     .replace('{{MESSAGE}}', data.message)
 
   const mailOptions = {
-    from: `"${data.name}" <${process.env.SMTP_FROM}>`,
+    from: `"Zivarri Jewels" <${process.env.SMTP_FROM}>`,
     to: process.env.CONTACT_EMAIL,
+    replyTo: `"${data.name}" <${data.email}>`,
     subject: `Zivarri.com: New contact form submission from ${data.name}`,
     html
   }
-  await transporter.verify()
-  await transporter.sendMail(mailOptions)
+  if (useProductionTransporter) await transporter.verify()
+  const info = await transporter.sendMail(mailOptions)
+
+  if (!useProductionTransporter) {
+    console.log('Message sent: %s', info.messageId)
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+  }
 }
 
 export { sendEmail }
